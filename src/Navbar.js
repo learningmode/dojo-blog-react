c166067507a8502b0d52178fb5462e80d9164965
learningmode <learningmode151@gmail.com>
2021-01-23 15:41:42 +0530

    Navbar 1

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar=()=>{
+  return(
+    <nav>
+      <h3>React Dojo Blog</h3>
+      <div className="links">
+          <a href="/">Home</a>
+          <a href="/create">New Blog</a>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
