835d97e5cdaf55baa6e1a9b9a10025c072220b37
learningmode <learningmode151@gmail.com>
2021-01-24 16:13:56 +0530

    click event handler

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,16 @@
 const Home = ()=>{
+  const handleClick=(e)=>{
+    // e is event object
+    return console.log("Hello REact Ninja",e);
+  }
+  const handleClickAgain=(name,e)=>{
+    console.log("Hello",name,e.target);
+  }
   return (
     <div className="home">
         <h2>Homepage</h2>
+        <button onClick={handleClick}>Click Me</button>
+        <button onClick={(e)=>{handleClickAgain('Naruto',e)}}>Click Me Again</button>
     </div>
   )
 }
