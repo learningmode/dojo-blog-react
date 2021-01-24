04fc426450cd3244184bc05e288caa11b4fac249
learningmode <learningmode151@gmail.com>
2021-01-24 17:28:49 +0530

    Use State used

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,10 @@
+import {useState} from 'react';
+
 const Home = ()=>{
+  const [name,setName]= useState('Mario');
   const handleClick=(e)=>{
     // e is event object
+    setName('Luigi');
     return console.log("Hello REact Ninja",e);
   }
   const handleClickAgain=(name,e)=>{
@@ -9,6 +13,7 @@ const Home = ()=>{
   return (
     <div className="home">
         <h2>Homepage</h2>
+        <p>{name}</p>
         <button onClick={handleClick}>Click Me</button>
         <button onClick={(e)=>{handleClickAgain('Naruto',e)}}>Click Me Again</button>
     </div>
